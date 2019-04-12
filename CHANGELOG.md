03901bceff10b947125ec58c10b601b0fee02691
Martin Loetzsch <martin.loetzsch@gmail.com>
2019-04-12 23:04:15 +0200

    Release 2.0.0
    - Change MARA_XXX variables to functions to delay importing of imports
    - Move some imports into the functions that use them in order to improve loading speed
    
    **required changes**
    
    - Update `mara-app` to `>=2.0.0`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Changelog
 
+## 2.0.0
+
+- Change MARA_XXX variables to functions to delay importing of imports
+- Move some imports into the functions that use them in order to improve loading speed
+
+**required changes**
+
+- Update `mara-app` to `>=2.0.0`
+
+
 ## 1.0.1 (2019-03-13)
 
 Improve display of JSON columns
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='data-sets',
-    version='1.0.1',
+    version='2.0.0',
 
     description='Flask based UI for displaying & segmenting a single database table',
 
     install_requires=[
-        'mara-db>=3.2.0',
-        'mara-page>=1.3.0'
+        'mara-db>=4.0.0',
+        'mara-page>=1.4.1'
     ],
 
     dependency_links=[
