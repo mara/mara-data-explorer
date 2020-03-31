454947f49ed219f99bd538848899d579481eb13a
Jieying Yan <jieying.yan@project-a.com>
2020-03-31 13:51:20 +0200

    include new data type time with / without time zone

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='data-sets',
-    version='2.2.1',
+    version='2.2.2',
 
     description='Flask based UI for displaying & segmenting a single database table',
 
